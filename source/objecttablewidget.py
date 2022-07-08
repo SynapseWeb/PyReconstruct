a9d40cc2f290ab31294b56e17705df06a445ed86
Julian Falco <julian.falco@utexas.edu>
2022-07-08 18:28:42 -0500

    Object list features

--- source/objecttablewidget.py
+++ source/objecttablewidget.py
@@ -1,59 +1,78 @@
-import json
-from PySide2.QtWidgets import QDockWidget, QTableWidget, QTableWidgetItem, QAbstractItemView, QHeaderView
+import orjson
+from PySide2.QtWidgets import QDockWidget, QTableWidget, QTableWidgetItem, QAbstractItemView, QMenuBar, QProgressDialog, QWidget
 from PySide2.QtGui import QTransform
-from PySide2.QtCore import Qt
+from PySide2.QtCore import Qt, QModelIndex
 
 from objecttableitem import ObjectTableItem
 
 class ObjectTableWidget(QDockWidget):
 
-    def __init__(self, series, wdir, quantities, progbar, parent):
+    def __init__(self, series, wdir, quantities, parent):
         super().__init__(parent)
         self.series = series
         self.wdir = wdir
         self.quantities = quantities
+        self.parent_widget = parent
 
         self.setFloating(True)
         self.setAllowedAreas(Qt.NoDockWidgetArea)
         self.setWindowTitle("Object List")
+        self.central_widget = QWidget()
+        self.setWidget(self.central_widget)
 
+        self.menubar = QMenuBar(self.central_widget)
+        self.listmenu = self.menubar.addMenu("List")
+        self.refresh_act = self.listmenu.addAction("Refresh")
+        self.refresh_act.triggered.connect(self.refresh)
+        self. findfirst_act = self.menubar.addAction("Find 1st!")
+        self.findfirst_act.triggered.connect(self.findFirst)
+        
+
+        self.createTable()
+
+        self.show()
+    
+    def createTable(self):
+        progbar = QProgressDialog("Loading object data...", "Cancel", 0, 100, self.parent_widget)
+        progbar.setWindowTitle("Object Data")
+        progbar.setWindowModality(Qt.WindowModal)
         self._objdict = {}
         self.loadSeriesData(progbar)
         if progbar.wasCanceled(): return
 
         horizontal_headers = ["Name"]
-        if quantities["range"]:
+        if self.quantities["range"]:
             horizontal_headers.append("Start")
             horizontal_headers.append("End")
-        if quantities["count"]:
+        if self.quantities["count"]:
             horizontal_headers.append("Count")
-        if quantities["surface_area"]:
+        if self.quantities["surface_area"]:
             horizontal_headers.append("Surface Area")
-        if quantities["flat_area"]:
+        if self.quantities["flat_area"]:
             horizontal_headers.append("Flat Area")
-        if quantities["volume"]:
+        if self.quantities["volume"]:
             horizontal_headers.append("Volume")
-        self.table = QTableWidget(len(self._objdict), len(horizontal_headers))
+        self.table = QTableWidget(len(self._objdict), len(horizontal_headers), self.central_widget)
         row = 0
         for name in sorted(self._objdict.keys()):
             trace_obj = self._objdict[name]
             self.table.setItem(row, 0, QTableWidgetItem(name))
             col = 1
-            if quantities["range"]:
+            if self.quantities["range"]:
                 self.table.setItem(row, col, QTableWidgetItem(str(trace_obj.start)))
                 col += 1
                 self.table.setItem(row, col, QTableWidgetItem(str(trace_obj.end)))
                 col += 1
-            if quantities["count"]:
+            if self.quantities["count"]:
                 self.table.setItem(row, col, QTableWidgetItem(str(trace_obj.count)))
                 col += 1
-            if quantities["surface_area"]:
+            if self.quantities["surface_area"]:
                 self.table.setItem(row, col, QTableWidgetItem(str(round(trace_obj.surface_area, 6))))
                 col += 1
-            if quantities["flat_area"]:
+            if self.quantities["flat_area"]:
                 self.table.setItem(row, col, QTableWidgetItem(str(round(trace_obj.flat_area, 6))))
                 col += 1
-            if quantities["volume"]:
+            if self.quantities["volume"]:
                 self.table.setItem(row, col, QTableWidgetItem(str(round(trace_obj.volume, 6))))
                 col += 1
             row += 1
@@ -63,12 +82,12 @@ class ObjectTableWidget(QDockWidget):
         self.table.verticalHeader().hide()
         self.table.resizeColumnsToContents()
         self.table.resizeRowsToContents()
-        total_w = 10
+        w = 10
         for i in range(self.table.columnCount()):
-            total_w += self.table.columnWidth(i)
-        self.setWidget(self.table)
-        self.resize(total_w, parent.height())
-        self.show()
+            w += self.table.columnWidth(i)
+        h = self.parent_widget.height()
+        self.resize(w, h)
+        self.table.setGeometry(0, 20, w, h-20)
 
     def loadSeriesData(self, progbar):
         self._objdict = {}
@@ -77,8 +96,8 @@ class ObjectTableWidget(QDockWidget):
         for i in range(len(self.series.sections)):
             section = self.series.sections[i]
             section_num = int(section[section.rfind(".")+1:])
-            with open(self.wdir + section, "r") as section_file:
-                section_data = json.load(section_file)
+            with open(self.wdir + section, "rb") as section_file:
+                section_data = orjson.loads(section_file.read())
             section_thickness = section_data["thickness"]
             t = section_data["tform"]
             point_tform = QTransform(t[0], t[3], t[1], t[4], t[2], t[5])
@@ -94,3 +113,14 @@ class ObjectTableWidget(QDockWidget):
             prog_value += 1
             progbar.setValue(prog_value / final_value * 100)
             if progbar.wasCanceled(): return
+    
+    def refresh(self):
+        self.createTable()
+    
+    def findFirst(self):
+        selected_indexes = self.table.selectedIndexes()
+        if len(selected_indexes) != 1 or selected_indexes[0].column() != 0:
+            return
+        r = selected_indexes[0].row()
+        obj_name = self.table.item(r, 0).text()
+        print(obj_name)

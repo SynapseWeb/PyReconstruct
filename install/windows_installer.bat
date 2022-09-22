f6a30e1fbcd6e640025ea2a60e4cdfe7a4fd0db3
Michael Chirillo <m.chirillo@utexas.edu>
2022-09-22 12:07:40 +0200

    Change batch requirements.txt filepath

--- install/windows_installer.bat
+++ install/windows_installer.bat
@@ -5,7 +5,7 @@ START /B /WAIT cmd /c python -m venv env
 call env\Scripts\activate
 @timeout /t 1 /nobreak > nul
 echo Installing dependencies...
-START /B /WAIT cmd /c pip install -r requirements.txt
+START /B /WAIT cmd /c pip install -r src/requirements.txt
 call deactivate
 cd install
 echo Finished.

b3dfda48014e545c24b266478de2c52fe8fc4373
michael <michael.a.chirillo@gmail.com>
2022-09-22 12:02:42 +0200

    Add batch installer for windows

---
+++ install/windows_installer.bat
@@ -0,0 +1,10 @@
+@echo off
+cd ..
+echo Creating virtual environment...
+START /B /WAIT cmd /c python -m venv env
+call env\Scripts\activate
+@timeout /t 1 /nobreak > nul
+echo Installing dependencies...
+START /B /WAIT cmd /c pip install -r requirements.txt
+call deactivate
+echo Finished.

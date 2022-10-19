c6bb9e39e41d8d8f0f7e36d7951c0e890ec01f65
Julian Falco <julian.falco@utexas.edu>
2022-10-19 09:37:54 -0600

    Add xml json conversions

---
+++ src/modules/gui/backend/xml_json_conversions.py
@@ -0,0 +1,78 @@
+import os
+
+from constants.blank_legacy_files import blank_series, blank_section
+
+from modules.recon.series import Series
+from modules.recon.section import Section
+
+def xmlToJSON(series : Series, new_dir : str):
+    """Convert a series in XML to JSON.
+    
+        Params:
+            series (Series): the series to convert
+            new_dir (str): the directory to store the new files
+    """
+    # save sections as JSON
+    for snum in series.sections:
+        section = series.loadSection(snum)
+        section.filetype = "JSON"
+        section.filepath = os.path.join(
+            new_dir,
+            os.path.basename(section.filepath)
+        )
+        section.save()
+    # save series as XML
+    series.filetype = "JSON"
+    series.filepath = os.path.join(
+        new_dir,
+        os.path.basename(series.filepath)
+    )
+    series.save()
+
+def jsonToXML(series : Series, new_dir : str):
+    """Convert a series in JSON to XML.
+    
+        Params:
+            series (Series): the series to convert
+            new_dir (str): the directory to store the new files
+    """
+    # save sections as XML
+    for snum in series.sections:
+        json_section = series.loadSection(snum)
+        # create a blank xml section
+        xml_text = blank_section
+        xml_text.replace("[SECTION_NUM]", str(snum))
+        xml_text.replace("[SECTION_THICKNESS]", str(json_section.thickness))
+        xml_text.replace("[TRANSFORM_DIM]", "3")
+        xml_text.replace("[XCOEF]", "0 1 0 0 0 0") # to be replaced
+        xml_text.replace("[YCOEF]", "0 0 1 0 0 0") # to be replaced
+        xml_text.replace("[IMAGE_MAG]", json_section.mag)
+        xml_text.replace("[IMAGE_SOURCE]", json_section.src)
+        xml_text.replace("[IMAGE_LENGTH]", "100000")
+        xml_text.replace("[IMAGE_HEIGHT]", "100000")
+        # save the file
+        new_path = os.path.join(
+            new_dir,
+            os.path.basename(json_section.filepath)
+        )
+        with open(new_path, "w") as xml_file:
+            xml_file.write(xml_text)
+        # load the xml section
+        xml_section = Section(new_path)
+        xml_section.tform = json_section.tform
+        xml_section.traces = json_section.traces
+        xml_section.save()
+    
+    # save series as xml
+    xml_text = blank_series
+    xml_text.replace("[SECTION_NUM]", str(series.sections.keys()[0]))
+    new_path = os.path.join(
+        new_dir,
+        os.path.basename(series.filepath)
+    )
+    with open(new_path, "w") as xml_file:
+        xml_file.write(xml_text)
+
+
+
+        

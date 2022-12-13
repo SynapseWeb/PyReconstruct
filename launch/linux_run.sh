47ac018f6d1969db71717b7a65a8493d71368246
Michael Chirillo <m.chirillo@utexas.edu>
2022-12-13 08:56:45 +0100

    Add linux launch script

---
+++ launch/linux_run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR/..
+
+if [ ! -f "env/bin/activate" ]; then
+    python -m venv env
+    source env/bin/activate
+    pip install --upgrade pip
+    deactivate
+fi
+
+source env/bin/activate
+pip install -r src/requirements.txt
+python src/pyReconstruct.py

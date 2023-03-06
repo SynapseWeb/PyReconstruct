850eaed79afb252bdbd5a539302218ebacd31463
Michael Chirillo <m.chirillo@utexas.edu>
2023-03-06 18:51:13 +0100

    Add symlinking for Linux launch script

--- launch/linux/run.sh
+++ launch/linux/run.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DIR=$( cd -- "$( dirname -- "$(readlink -f "${BASH_SOURCE[0]}" || ${BASH_SOURCE[0]})" )" &> /dev/null && pwd )
 
-cd $SCRIPT_DIR/..
+# Get correct python command
+if python --version 2>&1 | grep -q '^Python 3.10'; then
+    PY_CMD=python
+else
+    PY_CMD=Python3.10
+fi
+
+cd $DIR/../..
 
 if [ ! -f "env/bin/activate" ]; then
-    python -m venv env
+    $PY_CMD -m venv env
     source env/bin/activate
     pip install --upgrade pip
     deactivate
@@ -13,4 +20,4 @@ fi
 
 source env/bin/activate
 pip install -r src/requirements.txt
-python src/PyReconstruct.py
+$PY_CMD src/PyReconstruct.py

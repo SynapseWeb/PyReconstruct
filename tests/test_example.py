d9f8ffac66688e4bd30cdddf90d39b4f3cf8847c
Michael Chirillo <michael.a.chirillo@gmail.com>
2023-04-25 07:34:51 +0200

    Correct import statements and append sys.path

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,23 +2,32 @@
 
 import os, sys, shutil
 from PySide6.QtWidgets import QApplication
-from src.modules.gui.main import MainWindow
-from src.modules.constants.locations import checker_dir
+
+test_dir = os.path.dirname(__file__)
+run_test_dir = os.path.join(test_dir, 'run')
+series_dir = os.path.join(test_dir, 'series')
+
+src_dir = os.path.join(test_dir, '../src')
+
+if src_dir not in sys.path:
+    sys.path.append(src_dir)
+
+import modules.gui.main as main
 
 app = QApplication(sys.argv)
 
-# TEST 1: Open jser file (most current format)
-
-jser_current = os.path.join(testing_dir, "shapes1_broke.jser")
+# # TEST 1: Open jser file (most current format)
 
 # Copy temp testing files
-if os.path.exists(testing_dir): shutil.rmtree(testing_dir)
-shutil.copytree(backup_dir, testing_dir)
+if os.path.exists(run_test_dir): shutil.rmtree(run_test_dir)
+shutil.copytree(series_dir, run_test_dir)
+
+jser_current = os.path.join(run_test_dir, "shapes1.jser")
 
 # Run test
 try:
     
-    mainwindow = MainWindow([])  # invoke mainwindow (opens welcome series)
+    mainwindow = main.MainWindow([])  # invoke mainwindow (opens welcome series)
     mainwindow.openSeries(jser_fp=jser_current)  # open jser
     mainwindow.series.modified = False  # override save
     mainwindow.close()  # close series
@@ -29,5 +38,4 @@ except:
     print('Test 1 (open current jser file): fail')
 
 # Clean up dirs
-shutil.rmtree(testing_dir)
-
+shutil.rmtree(run_test_dir)

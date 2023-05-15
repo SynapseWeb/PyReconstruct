0b68536a62190836e646a97ebc7d12a2f334ce59
Michael Chirillo <michael.a.chirillo@gmail.com>
2023-05-15 10:14:24 -0400

    Incorporate autoseg opts into predict

--- src/modules/gui/dialog/predict.py
+++ src/modules/gui/dialog/predict.py
@@ -18,12 +18,13 @@ from modules.gui.utils import notify
 
 class PredictDialog(QDialog):
 
-    def __init__(self, parent, models : dict):
+    def __init__(self, parent, models : dict, opts : dict):
         """Create a dialog for predicting an autoseg model.
         
             Params:
                 parent (QWidget): the parent of the dialog
                 models (dict): the dictionary containing the model paths
+                opts (dict): a dictionary containin autoseg options
         """
         super().__init__(parent)
 
@@ -31,6 +32,9 @@ class PredictDialog(QDialog):
 
         zarr_fp_text = QLabel(self, text="Zarr")
         self.zarr_fp_input = BrowseWidget(self, type="dir")
+
+        if opts.get("zarr_current"):
+            self.zarr_fp_input.le.setText(opts.get("zarr_current"))
 
         self.models = models
         model_text = QLabel(self, text="Model")
@@ -41,20 +45,40 @@ class PredictDialog(QDialog):
                 items.append(f"{g} - {m}")
         self.model_input.addItems(items)
 
+        if opts.get("model_path"):
+            original_path = opts.get("model_path")
+            original_choice = os.path.basename(original_path)[:-3]
+            original_type = os.path.basename(os.path.dirname(original_path))
+            orig = f'{original_type} - {original_choice}'
+            self.model_input.setCurrentIndex(items.index(orig))
+
         cfile_text = QLabel(self, text="Checkpoint:")
         self.cfile_input = BrowseWidget(self, type="file")
+
+        if opts.get("cp_path"):
+            self.cfile_input.le.setText(opts.get("cp_path"))
 
         write_text = QLabel(self, text="Write")
         self.write_input = QComboBox(self)
         write_opts = ["affs", "lsds", "mask", "all"]
         self.write_input.addItems(write_opts)
 
+        current_write = opts.get("write")
+        if current_write:
+            self.write_input.setCurrentIndex(write_opts.index(current_write))
+
         increase_text = QLabel(self, text="Increase")
         self.increase_input = QLineEdit(self)
         self.increase_input.setText("")
 
+        if opts.get("increase"):
+            self.increase_input.setText(opts.get("increase"))
+
         downsample_text = QLabel(self, text="Downsample")
         self.downsample_input = QCheckBox(self)
+
+        if opts.get("downsample") == True:
+            self.downsample_input.setChecked(True)
         
         layout = QGridLayout()
 

fcaf2ffa0d759619f1846aa8d40dc57abcec801f
Julian Falco <julian.falco@utexas.edu>
2023-05-25 13:46:03 -0600

    Add restart function

--- src/PyReconstruct.py
+++ src/PyReconstruct.py
@@ -1,13 +1,11 @@
+import os
 import sys
+import PySide6
+from pathlib import Path
 from PySide6.QtWidgets import QApplication
-from modules.gui.main import MainWindow
 
 # STOPGAP FOR WAYLAND QT ISSUE
 # https://stackoverflow.com/questions/68417682/qt-and-opencv-app-not-working-in-virtual-environment
-
-import os
-import PySide6
-from pathlib import Path
 
 ps6_fp = PySide6.__file__
 plugin_path = os.fspath(Path(ps6_fp).resolve().parent / "Qt" / "plugins")
@@ -15,7 +13,17 @@ os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = plugin_path
 
 # END STOPGAP
 
-# Create and run applications
+# create the Qt Application
 app = QApplication(sys.argv)
-main_window = MainWindow(sys.argv)
-app.exec()
+
+# run program until user closes without restart
+run = True
+while run:
+    from modules.gui.main import MainWindow
+    main_window = MainWindow(sys.argv)
+    app.exec()
+    if main_window.restart_mainwindow:
+        if not main_window.series.isWelcomeSeries():
+            sys.argv[1] = main_window.series.jser_fp  # load the series being worked on
+    else:
+        run = False

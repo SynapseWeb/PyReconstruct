54edbfddccb0696cd7cac92debf36cc4703d0000
Julian Falco <julian.falco@utexas.edu>
2023-09-19 14:07:55 -0600

    Modify importing options

--- src/modules/gui/table/trace.py
+++ src/modules/gui/table/trace.py
@@ -211,11 +211,14 @@ class TraceTableWidget(QDockWidget):
                 return False
         
         # check regex
+        passes_filters = False if self.re_filters else True
         for re_filter in self.re_filters:
             if bool(re.fullmatch(re_filter, name)):
-                return True
+                passes_filters = True
+        if not passes_filters:
+            return False
         
-        return False
+        return True
 
     def getFilteredObjects(self):
         """Get the names of the objects that pass the filter."""

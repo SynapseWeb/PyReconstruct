ddd46759cc1832dfef6d7bb52409bb98aa47403c
Michael Chirillo <michael.a.chirillo@gmail.com>
2023-12-14 15:16:28 +0100

    Add makefiles for development environment

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
+import os
 import pathlib
 from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-# It can be convenient to put the metadata for a package at the top
-# of a file
 VERSION = '0.1.0'
 PACKAGE_NAME = 'PyReconstruct'
 AUTHOR = 'Julian Falco & Michael Chirillo'
@@ -13,17 +12,9 @@ URL = 'https://github.com/SynapseWeb/PyReconstruct'
 LICENSE = 'None'
 DESCRIPTION = 'A version of RECONSTRUCT written in Python'
 
-# PyPI supports a "long description," which basically a README
-# file that is published alongside your package metadata.
-# The code here shows how to dynamically load the text of 
-# your package README and provide it for the long description.
-# The example in this article uses Mardown for readme and 
-# documentation, other supported formats include
-# Restructured Text (RST).
 LONG_DESCRIPTION = (HERE / "readme.md").read_text()
 LONG_DESC_TYPE = "text/markdown"
 
-# Dependencies for the package
 INSTALL_REQUIRES = [
       'PySide6',
       'opencv-python',
@@ -35,9 +26,9 @@ INSTALL_REQUIRES = [
       'lxml'
 ]
 
-# Initialize setup with metadata and package dependencies.
-# find_packages is used to recourse the structure of the 
-# project and dynamically generate the package
+
+SHELL_SCRIPTS = []  # List shell scripts to install
+
 setup(
     name=PACKAGE_NAME,
     version=VERSION,
@@ -51,6 +42,7 @@ setup(
     install_requires=INSTALL_REQUIRES,
     packages=find_packages(),
     package_data={'PyReconstruct': ['assets/**/*', 'assets/welcome_series/.welcome/*']},
+    scripts=SHELL_SCRIPTS,
     entry_points={
         'console_scripts': [
             'PyReconstruct=PyReconstruct.cli:main'

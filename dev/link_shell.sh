66d5a0642437c5db9acda5cbc1f9d0b72b40ba4f
Michael Chirillo <m.chirillo@utexas.edu>
2023-12-15 11:45:13 +0100

    Add hooks to dev environment make process

--- dev/link_shell.sh
+++ dev/link_shell.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/sh
 
-ENV_BIN=$1
-SHELL_SCRIPTS=$(ls shell)
+ENV_DIR=$1
+ENV_BIN=$ENV_DIR/bin
+ENV_ETC=$ENV_DIR/etc/conda
+
+#### SHELL SCRIPTS #############################################################
 
 echo 'Linking shell scripts to environment bin...'
+
+SHELL_SCRIPTS=$(ls shell)
 
 for script in $SHELL_SCRIPTS ; do
   ENV_SCRIPT=$ENV_BIN/$script
@@ -13,3 +18,17 @@ for script in $SHELL_SCRIPTS ; do
 done
 
 echo 'Shell scripts available in environment bin directory.'
+
+#### HOOKS #####################################################################
+
+echo 'Adding to pre/post-activate hooks...'
+
+for script in env_tweaks/activate/* ; do
+  cp $script $ENV_ETC/activate.d/
+done
+
+for script in env_tweaks/deactivate/* ; do
+  cp $script $ENV_ETC/deactivate.d/
+done
+
+echo 'Hooks amended...'

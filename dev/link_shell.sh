98dfc552862ec669354ec854a16067fd078ec24d
Michael Chirillo <michael.a.chirillo@gmail.com>
2023-12-23 13:46:18 +0100

    Correct env python bin while appending to conda.pth

--- dev/link_shell.sh
+++ dev/link_shell.sh
@@ -38,7 +38,7 @@ echo 'Hooks amended.'
 echo 'Adding repo to path...'
 
 REPO_PATH=$(realpath ..)
-PYTHON=$(python -c "import sys; print(f'python{sys.version_info.major}.{sys.version_info.minor}')")
+PYTHON=$($ENV_BIN/python -c "import sys; print(f'python{sys.version_info.major}.{sys.version_info.minor}')")
 CONDA_PTH=$ENV_DIR/lib/$PYTHON/site-packages/conda.pth
 echo $REPO_PATH >> $CONDA_PTH
 

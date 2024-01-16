8b47fd5b04f860801c919406e80193e1cce2ddf2
Julian Falco <julian.falco@utexas.edu>
2024-01-16 14:35:15 -0700

    Remove test print statements

--- PyReconstruct/modules/gui/main/field_widget.py
+++ PyReconstruct/modules/gui/main/field_widget.py
@@ -55,8 +55,6 @@ class FieldWidget(QWidget, FieldView):
         self.mainwindow = mainwindow
         self.setMouseTracking(True)
 
-        print("test2")
-
         # enable touch gestures
         Qt.WA_AcceptTouchEvents = True
         gestures = [Qt.GestureType.PinchGesture]
--- PyReconstruct/modules/gui/main/main_window.py
+++ PyReconstruct/modules/gui/main/main_window.py
@@ -61,8 +61,6 @@ class MainWindow(QMainWindow):
         super().__init__() # initialize QMainWindow
         self.setWindowTitle("PyReconstruct")
 
-        print("test1")
-
         # catch all exceptions and display error message
         def customExcepthook(exctype, value, traceback):
             """

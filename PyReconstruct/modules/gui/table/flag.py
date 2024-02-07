c21d80a2a6f8da07d590f583f9bf2026589e2899
Julian Falco <julian.falco@utexas.edu>
2024-02-07 15:22:23 -0700

    fix(lists): reimplement # to [0-9] in regex filters

--- PyReconstruct/modules/gui/table/flag.py
+++ PyReconstruct/modules/gui/table/flag.py
@@ -464,6 +464,7 @@ class FlagTableWidget(QDockWidget):
             return
         
         self.re_filters = set(response[0] if response[0] else [".*"])
+        self.re_filters = set([s.replace("#", "[0-9]") for s in self.re_filters])
 
         # call through manager to update self
         self.manager.updateTable(self)

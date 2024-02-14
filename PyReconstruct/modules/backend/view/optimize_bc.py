221ec4e2ba241eaa2922fdbcfd1d971cc861c19c
Julian Falco <julian.falco@utexas.edu>
2024-02-14 14:49:34 -0700

    fix(images): modify optimize b/c function to skip corrupt images

--- PyReconstruct/modules/backend/view/optimize_bc.py
+++ PyReconstruct/modules/backend/view/optimize_bc.py
@@ -72,8 +72,12 @@ def optimizeSectionBC(section : Section, desired_mean=128, desired_std=60, windo
     
     # get the image array
     if window is None:
-        image = cv2.imread(fp, cv2.IMREAD_GRAYSCALE)
-        cv2.resize(image, (1024, 1024))
+        try:
+            image = cv2.imread(fp, cv2.IMREAD_GRAYSCALE)
+            cv2.resize(image, (1024, 1024))
+        except:
+            print(f"Image at {fp} is corrupt. Skipping...")
+            return
     else:
         slayer = SectionLayer(section, section.series)
         pixmap_dim = round(window[2] / section.mag), round(window[3] / section.mag)

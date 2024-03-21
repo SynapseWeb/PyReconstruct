5394d4b2ba0237ecbb6ffeb6d5cb20e66176ff38
Michael Chirillo <m.chirillo@utexas.edu>
2024-03-21 09:09:31 +0600

    feat(help): add access to GitHub repo and issues from help menu

---
+++ PyReconstruct/modules/constants/websites.py
@@ -0,0 +1,6 @@
+kh_web = "https://synapseweb.clm.utexas.edu/harrislab"
+kh_wiki = "https://wikis.utexas.edu/display/khlab/PyReconstruct+user+guide"
+kh_atlas = "https://synapseweb.clm.utexas.edu/atlas"
+gh_repo = "https://github.com/SynapseWeb/PyReconstruct"
+gh_issues = gh_repo + "/issues"
+gh_submit = gh_issues + "/new/choose"

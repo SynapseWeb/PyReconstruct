8b5e1bd18879e5563d496b9b169ae7b822311b4d
Michael Chirillo <m.chirillo@utexas.edu>
2024-03-28 08:38:11 +0600

    refactor(start): simplify and update run script

--- PyReconstruct/run.py
+++ PyReconstruct/run.py
@@ -1,53 +1,65 @@
-import os
-import sys
-import PySide6
-import importlib
+import os, sys, importlib
 from pathlib import Path
+
+import PySide6
 from PySide6.QtWidgets import QApplication
 
+
 if __name__ == "__main__":
-    # necessary to import correctly given run.py location
-    here_dir = os.path.dirname(os.path.realpath(__file__))
-    sys.path.append(
-        os.path.dirname(here_dir)
-    )
+    
+    # set up imports for run.py location
+    run_script = Path(__file__)
+    pypath = str(run_script.parents[1])
+    sys.path.append(pypath)
+
 
 import PyReconstruct.modules.gui.main as main
 
+
 def runPyReconstruct(filename=None):
-    # STOPGAP FOR WAYLAND QT ISSUE
-    # https://stackoverflow.com/questions/68417682/qt-and-opencv-app-not-working-in-virtual-environment
 
-    ps6_fp = PySide6.__file__
-    plugin_path = os.fspath(Path(ps6_fp).resolve().parent / "Qt" / "plugins")
-    os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = plugin_path
-
-    # END STOPGAP
+    # Stopgap for Wayland Qt issue
+    # stackoverflow.com/questions/68417682/qt-and-opencv-app-not-working-in-virtual-environment
+    ps6_dir = Path(PySide6.__file__).parent
+    qt_plugins = ps6_dir / "Qt/plugins"
+    os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = str(qt_plugins)
 
     # create the Qt Application
     app = QApplication(sys.argv)
 
     # run program until user closes without restart
     run = True
+
     while run:
+
         main_window = main.MainWindow(filename)
         app.exec()
 
         # user has closed the main window
-        if main_window.restart_mainwindow:
+        if main_window.restart_mainwindow:  # restart requested
+
             if not main_window.series.isWelcomeSeries():
                 filename = main_window.series.jser_fp
-            # reload all of the written code
-            for module_name, module in list(sys.modules.items()):
+
+            # reload PyReconstruct modules
+            loaded_modules = list(sys.modules.items())
+            for module_name, module in loaded_modules:
                 if module_name.startswith("PyReconstruct.modules"):
                     importlib.reload(module)
-        else:
+                    
+        else:  # no restart requested
+
             run = False
 
+
 if __name__ == "__main__":
-    # get filename from argv
-    if len(sys.argv) > 1:
+
+    if len(sys.argv) > 1:  # get filename from argv
+
         filename = sys.argv[1]
+
     else:
+        
         filename = None
+        
     runPyReconstruct(filename)

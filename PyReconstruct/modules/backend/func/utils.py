346bec134e3df348783be615e85def3d5b890d98
Michael Chirillo <m.chirillo@utexas.edu>
2025-01-23 08:25:25 -0600

    feat(utils): add function to redirect stdout to devnull

--- PyReconstruct/modules/backend/func/utils.py
+++ PyReconstruct/modules/backend/func/utils.py
@@ -1,5 +1,7 @@
 import os
 import uuid
+from contextlib import redirect_stdout
+
 
 def make_unique_id() -> int:
     """Return a uuid."""
@@ -13,3 +15,18 @@ def determine_cpus(percent_usage: int) -> int:
     cpus = int(os.cpu_count() * (percent_usage / 100))
 
     return cpus or 1
+
+
+def stdout_to_devnull(func):
+    """Silence stdout by redirecting to devnull.
+
+    Use as @stdout_to_devnull decorator or as stdout_to_devnull(func)(args)
+    """
+
+    def wrapper(*args, **kwargs):
+        
+        with open(os.devnull, 'w') as devnull:
+            with redirect_stdout(devnull):
+                return func(*args, **kwargs)
+            
+    return wrapper
